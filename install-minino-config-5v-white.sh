8d01c6783d0de29e937bc2fe142326e3a179d2d1
aosucas499 <aosucas499@gmail.com>
2021-03-02 22:16:14 +0100

    Update install-minino-config-5v-white.sh

--- install-minino-config-5v-white.sh
+++ install-minino-config-5v-white.sh
@@ -7,6 +7,23 @@ sudo apt-get update -y
 sudo apt-get install pavucontrol -y
 sudo apt-get install crda -y
 
+# Remover paquetes de firmware incompatibles con la instalación del firmare de la tablet o kernel
+#cd /home/$USER
+sudo apt-get purge --remove atmel-firmware -y
+sudo apt-get purge --remove firmware-libertas -y
+sudo apt-get purge --remove firmware-misc-nonfree -y
+sudo apt-get purge --remove firmware-myricom -y
+sudo apt-get purge --remove firmware-iwlwifi -y
+sudo apt-get purge --remove firmware-atheros -y
+sudo apt-get purge --remove firmware-ipw2x00 -y
+sudo apt-get purge --remove firmware-linux-free -y
+sudo apt-get purge --remove firmware-realtek -y
+sudo apt-get purge --remove firmware-intelwimax -y
+sudo apt-get purge --remove firmware-brcm80211 -y
+sudo apt-get purge --remove firmware-netxen -y
+sudo apt-get purge --remove firmware-bnx2x -y
+sudo apt-get purge --remove firmware-amd-graphics -y
+
 # Descargamos ambos kernels de las tablet vexia
 #
 #wget http://centros.edu.guadalinex.org/Edu/precisedda/pool/main/l/linux-firmware/linux-firmware_1.79.1_all.deb
@@ -21,13 +38,16 @@ wget http://centros.edu.guadalinex.org/Edu/catcornerdda/pool/main/g/guadalinexed
 sudo dpkg -i guadalinexedu-keyring_0.1-10_all.deb
 sudo mv /etc/apt/sources.list /etc/apt/sources.list.save
 sudo echo "deb http://centros.edu.guadalinex.org/Edu/precisedda2 precise main" > sources.list
+sudo echo "deb http://centros.edu.guadalinex.org/Edu/precisedda precise main" >> sources.list
 sudo echo "deb http://centros.edu.guadalinex.org/Edu/catcornerdda2 guadalinexedu main" >> sources.list
+sudo echo "deb http://centros.edu.guadalinex.org/Edu/catcornerdda guadalinexedu main" >> sources.list
+sudo echo "deb http://centros.edu.guadalinex.org/Edu/catcornersc guadalinexedu main" >> sources.list
 sudo cp sources.list /etc/apt
 sudo apt-get update -y
 
-## Supuestamente El kernel 3.4.43 es para la tablet con cargador negro a 9V
+## El kernel 3.4.43 es para la tablet con cargador negro a 9V
 #
-## Supuestamente El kernel 3.10.20 es para la tablet con cargador blanco a 5V
+## El kernel 3.10.20 es para la tablet con cargador blanco a 5V
 #
 
 #Instalamos kernel 5V
@@ -37,10 +57,15 @@ sudo apt-get update -y
 #sudo mount -av
 #sudo apt-get update -y
 #sudo apt-get install -f -y
+sudo apt-get install -y linux-firmware
 sudo apt-get install -y linux-image-3.10.20-generic-pae
 sudo mv /mnt/efi/guadalinex.img /boot/
 sudo mv /mnt/efi/kernel /boot/
 
+## Generamos arranque para 5v diferente al de fábrica
+#
+sudo cp boot/linux-5v/vmlinuz-3.10.20_edu /boot
+sudo update-initramfs -d -k 3.10.20_edu
 
 ## Generamo grub
 #
@@ -49,19 +74,19 @@ sudo update-grub2
 
 #Borramos imagen linux grub-pc
 #
-sudo rm /boot/initrd*
-sudo rm /boot/Sys*
-sudo rm /boot/vmlinuz*
-sudo rm /boot/confi*
-sudo rm -r /mnt/efi
+#sudo rm /boot/initrd*
+#sudo rm /boot/Sys*
+#sudo rm /boot/vmlinuz*
+#sudo rm /boot/confi*
+#sudo rm -r /mnt/efi
 
 #Deshabilitamos la ganancia del sonido de guadalinex que en minino suena horrible
 #
 #sudo sed -i '99 s/^/#/g' /etc/rc.local
 
 #Deshabilitamos la cámara trasera, ya que aunque se cambien configuraciones, siempre monta la trasera.
-sudo sed -i '$ i\rm /dev/video0' /etc/rc.local
-sudo sed -i '$ i\ln -s /dev/video0 /dev/video1' /etc/rc.local
+#sudo sed -i '$ i\rm /dev/video0' /etc/rc.local
+#sudo sed -i '$ i\ln -s /dev/video0 /dev/video1' /etc/rc.local
 
 
 #Drivers táctil guadalinex
