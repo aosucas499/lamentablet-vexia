cf46674982d6bc1bff8e2a59baf774069d098ef9
aosucas499 <aosucas499@gmail.com>
2021-05-27 22:05:10 +0200

    mtrack driver testing

---
+++ multitouch/mtrack/install.sh
@@ -0,0 +1,31 @@
+!#/bin/bash
+
+#repo=BlueDragonX
+#repo=kurt-vd
+repo=aleixq
+
+sudo rm /usr/share/X11/xorg.conf.d/5-multitouch.conf
+sudo apt-get install libtool -y
+sudo apt-get install xinput -y
+wget https://www.x.org/archive//individual/util/util-macros-1.16.2.tar.gz
+tar -xf util-macros-1.16.2.tar.gz
+rm *.gz
+cd util-macros*
+./configure
+sudo make install
+sudo cp xorg-macros.m4 /usr/share/aclocal
+sudo ln -s /usr/bin/libtool /usr/share/aclocal/LIBTOOL
+cd ..
+git clone https://github.com/$repo/xf86-input-mtrack
+cd xf86*
+#solo si necesita cambiar a la branch absolute de aleixq
+	git checkout absolute
+autoreconf 
+automake
+./configure
+make
+sudo make install
+sudo cp /usr/local/lib/xorg/modules/input/* /usr/lib/xorg/modules/input
+cd ..
+sudo cp /4-mtrack.conf /usr/share/X11/xorg.conf.d/
+cd ~
